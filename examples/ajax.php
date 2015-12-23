cc2aeca14c575ac398e0a233a68cf53786971eb5
janhuang <384099566@qq.com>
2015-12-23 16:36:53 +0800

    add ajax request method and custom request headers

--- examples/ajax.php
+++ examples/ajax.php
@@ -2,17 +2,20 @@
 /**
  * Created by PhpStorm.
  * User: janhuang
- * Date: 15/6/12
- * Time: 下午11:31
- * Github: https://www.github.com/janhuang 
+ * Date: 15/12/23
+ * Time: 下午4:02
+ * Github: https://www.github.com/janhuang
  * Coding: https://www.coding.net/janhuang
  * SegmentFault: http://segmentfault.com/u/janhuang
  * Blog: http://segmentfault.com/blog/janhuang
  * Gmail: bboyjanhuang@gmail.com
+ * WebSite: http://www.janhuang.me
  */
 
 include __DIR__ . '/../vendor/autoload.php';
 
 $request = \FastD\Http\Request::createRequestHandle();
 
-echo json_encode(['name' => 'jan']);
+$response = $request->createRequest('http://localhost/me/fastd/library/http/examples/base.php')->ajax();
+echo '<pre>';
+print_r($response);
