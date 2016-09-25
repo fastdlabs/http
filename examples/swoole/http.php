fd3b0769f9824e3621b4cd0c66a4054e27657a5a
janhuang <bboyjanhuang@gmail.com>
2016-09-25 10:02:38 +0800

    add swoole handle

--- examples/swoole/http.php
+++ examples/swoole/http.php
@@ -7,5 +7,16 @@
  * @link      http://www.fast-d.cn/
  */
 
+use FastD\Http\SwooleServerRequest;
+
 include __DIR__ . '/../../vendor/autoload.php';
+
+$http = new swoole_http_server("127.0.0.1", 9501);
+
+$http->on('request', function ($request, $response) {
+    $server = SwooleServerRequest::createFormSwoole($request, $response);
+    $server->response($server->server->getPathInfo());
+});
+
+$http->start();
 
