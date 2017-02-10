1e613bf0a8f3f263d4abc1406734dc2c3eca65d7
Runnerlee <runnerleer@gmail.com>
2017-02-10 16:45:12 +0800

    add support raw data payload to httppost request

--- src/Request.php
+++ src/Request.php
@@ -270,19 +270,17 @@ class Request extends Message implements RequestInterface
     }
 
     /**
-     * @param array $data
+     * @param array|string $data
      * @param array $headers
      * @return Response
      */
-    public function send(array $data = [], array $headers = [])
+    public function send($data = [], array $headers = [])
     {
         $ch = curl_init();
         $url = (string)$this->uri;
 
-        $data = http_build_query($data);
-
         if (in_array($this->getMethod(), ['PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'])) {
-            $this->withOption(CURLOPT_POSTFIELDS, $data);
+            $this->withOption(CURLOPT_POSTFIELDS, is_array($data) ? http_build_query($data) : $data);
         } else if (!empty($data)) {
             $url .= (false === strpos($url, '?') ? '?' : '&') . $data;
         }
