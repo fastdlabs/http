4c6cbba7b1060280e93c94e208d8b41b7bbf8ab4
JanHuang <bboyjanhuang@gmail.com>
2018-12-29 16:26:42 +0800

    7.1

--- request.php
+++ request.php
@@ -6,3 +6,13 @@
  * @see      https://www.github.com/janhuang
  * @see      http://www.fast-d.cn/
  */
+
+include __DIR__ . '/vendor/autoload.php';
+
+$request = new \FastD\Http\Request('POST', new \FastD\Http\Uri('http://api.linghit.com/'));
+
+$response = $request->send();
+echo $response;
+$content = $response->getBody();
+$content = json_encode(json_decode($content, true), JSON_PRETTY_PRINT);
+echo $content;
